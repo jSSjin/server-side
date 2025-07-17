37d620cadaf55cd77bed7bbcede8542a13e186c4
jSSjin <jerat004@gmail.com>
2025-07-17 14:59:25 +0700

    add api page

--- demo-http-server.js
+++ demo-http-server.js
@@ -1,15 +1,31 @@
+const { timeStamp } = require('console');
 const  http = require('http');
 const  hostname = 'localhost';
 const  port = 3000;
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+
 
     if(req.url === '/about'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
         res.end('about page\n');
+
     }else if (req.url === '/contact'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
         res.end('Contact page\n');
+
+    }else if(req.url === '/api'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        const data = {
+            message: 'hello from api',
+            timeStamp: new Date().toISOString()
+        };
+        res.end(JSON.stringify(data));
     }else{
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
         res.end('hello, world!\n');
     }
 });
