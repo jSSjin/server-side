1c13afb5e466c9f09ce4df22a3fab84e0c64bd43
jSSjin <jerat004@gmail.com>
2025-07-17 14:48:39 +0700

    add contact and about

--- demo-http-server.js
+++ demo-http-server.js
@@ -4,7 +4,14 @@ const  port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, world!\n');
+
+    if(req.url === '/about'){
+        res.end('about page\n');
+    }else if (req.url === '/contact'){
+        res.end('Contact page\n');
+    }else{
+        res.end('hello, world!\n');
+    }
 });
 
 server.listen(port, hostname, () => {
