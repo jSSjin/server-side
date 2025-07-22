c26d57fbaf40ba164cb546078b401cb00aaafcc0
jSSjin <jerat004@gmail.com>
2025-07-22 21:15:02 +0700

    connect to db crreate container for db add GET method for api

--- HW2/app.js
+++ HW2/app.js
@@ -1,17 +1,56 @@
+require('dotenv').config();
+const mysql = require('mysql2');
 const express = require('express');
 const app = express();
-require('dotenv').config;
-
-app.get('/', (req, res) => {
-    res.send('Got a Get request');
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,      
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 });
 
-app.post('/', (req, res) => {
-    res.send('Got a Post request');
+app.get('/products', (req, res) => {
+    const sql = 'SELECT * FROM products';
+    db.query(sql, (err, result) => {
+        if(err){
+            res.status(500).json({message: 'Error occurred while retreving products', error: err });
+        }
+        else{
+            res.status(200).json(result);
+        }
+    });
 });
 
-app.put('/', (req, res) => {
-    res.send('Got a put request');
+app.get('/products/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const sql = 'SELECT * FROM products WHERE id = ?';
+    db.query(sql, [id], (err, result) => {
+        if(err){
+            res.status(500).json({message: 'Error occurred while retreving products', error: err });
+        }
+        else{
+            if(result.length === 0){
+                res.status(404).json({message: 'products not found', data: result})
+            }
+            else{
+                res.status(200).json({data: result});
+            }
+        }
+    });
+});
+
+app.get('/products/search/:keyword', (req, res) => {
+    const keyword = req.params.keyword;
+    const sql = 'SELECT * FROM products WHERE name LIKE ?';
+    db.query(sql, [`%${keyword}%`], (err, result) => {
+        if(err){
+            res.status(500).json({message: 'Error occurred while retreving products', error: err });
+        }
+        else{
+            res.status(200).json(result);
+        }
+    });
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
--- server-side-lecture/lec2/app.js
+++ server-side-lecture/lec2/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+
 app.get('/', (req, res) => {
     console.log(req.query)
     res.send(req.query.name + ' ' + req.query.age)
