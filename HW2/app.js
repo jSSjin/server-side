d88c118be0c3a1d777ab55c70016ece29c6654db
jSSjin <jerat004@gmail.com>
2025-07-29 20:18:44 +0700

    implement basic CRUD API and soft del + restore data

--- HW2/app.js
+++ HW2/app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const mysql = require('mysql2');
 const express = require('express');
 const app = express();
+app.use(express.json());
+
 const db = mysql.createConnection({
   host: process.env.DB_HOST,      
   port: process.env.DB_PORT,
@@ -11,7 +13,7 @@ const db = mysql.createConnection({
 });
 
 app.get('/products', (req, res) => {
-    const sql = 'SELECT * FROM products';
+    const sql = 'SELECT * FROM products WHERE is_deleted = 0';
     db.query(sql, (err, result) => {
         if(err){
             res.status(500).json({message: 'Error occurred while retreving products', error: err });
@@ -24,7 +26,7 @@ app.get('/products', (req, res) => {
 
 app.get('/products/:id', (req, res) => {
     const id = Number(req.params.id);
-    const sql = 'SELECT * FROM products WHERE id = ?';
+    const sql = 'SELECT * FROM products WHERE id = ? AND is_deleted = 0';
     db.query(sql, [id], (err, result) => {
         if(err){
             res.status(500).json({message: 'Error occurred while retreving products', error: err });
@@ -42,7 +44,7 @@ app.get('/products/:id', (req, res) => {
 
 app.get('/products/search/:keyword', (req, res) => {
     const keyword = req.params.keyword;
-    const sql = 'SELECT * FROM products WHERE name LIKE ?';
+    const sql = 'SELECT * FROM products WHERE name LIKE ? AND is_deleted = 0';
     db.query(sql, [`%${keyword}%`], (err, result) => {
         if(err){
             res.status(500).json({message: 'Error occurred while retreving products', error: err });
@@ -52,5 +54,45 @@ app.get('/products/search/:keyword', (req, res) => {
         }
     });
 });
+
+app.post('/products', (req, res) => {
+    const {name, price, discount, review_count, image_url} = req.body;
+    db.query('INSERT INTO products (name, price, discount, review_count, image_url) VALUES (?, ?, ?, ?, ?)',
+        [name, price, discount ,review_count, image_url],
+        (err, result) => {
+            if(err) return res.status(500).json({error: err.message});
+            res.status(201).json({ id: result.insertId, message: 'Product created'});
+        }
+    );
+
+});
+
+app.put('/products/:id', (req, res) => {
+    const {name, price, discount, review_count, image_url} = req.body;
+    db.query('UPDATE products SET name = ?, price = ?, discount = ?, review_count = ?, image_url = ? WHERE id = ?',
+        [name, price, discount ,review_count, image_url, req.params.id],
+        (err) => {
+            if(err) return res.status(500).json({error: err.message});
+            res.json({ message: 'products updated'})
+        }
+    );
+});
+
+//soft delete
+app.delete('/products/:id', (req, res) => {
+    db.query('UPDATE products SET is_deleted = 1 WHERE id = ?', [req.params.id], (err) => {
+        if(err) return res.status(500).json({error: err.message});
+        res.json({ message: 'product soft-deleted'});
+    });
+});
+
+//restore
+app.put('/products/restore/:id', (req, res) => {
+    db.query('UPDATE products SET is_deleted = 0 WHERE id =?', [req.params.id], (err) => {
+        if(err) return res.status(500).json({ err: err.message});
+        res.json({ message: 'Product restored'});
+    });
+});
+
 
 app.listen(3000, () => console.log('Server running on port 3000'));
